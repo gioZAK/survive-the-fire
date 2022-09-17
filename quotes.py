6ba69cdfa3c7cf64f4bc24189dcd9992871114ca
gioZAK <giovane.zakc@gmail.com>
2022-09-17 06:25:03 +0000

    Create the first question and quotes file

---
+++ quotes.py
@@ -0,0 +1,8 @@
+def quotes_1():
+    '''First Quote'''
+
+    print("\nDon't wait for other people to react or to get")
+    print("instructions from someone else. If anything seems wrong,")
+    print("if you smell smoke or you hear a smoke alarm,")
+    print("GET OUT, STAY OUT and CALL 112 or 999 (Ireland Only)")
+    print("from a safe location.")
--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 import time
 import sys
+import random
+from quotes import *
 
-def run_Game():
+POSITIVE = ["Good Job!", "Great!", "You are correct!", "😃"]
+NEGATIVE = ["Oh no!!!", "Try Again", "That's incorrect", "😔"]
+
+def run_game():
     '''Main function that runs the game'''
 
+    print("Loading...")
+    time.sleep(0.5)
+    
 
     def intro_text():
         """
         Function that runs a basic intruduction to the user.
         """ 
-
+        clear_screen()
         print("\nHello!")
         print("In this quick game you will test your knowledge about house fire...")
         print("Will you be able to surive??")
@@ -20,21 +28,57 @@ def run_Game():
         if user_choice.upper().strip() == "Y":
             clear_screen()
             print("Let's go!")
-            time.sleep(1.5)
+            time.sleep(2)
             first_question()
         elif user_choice.upper().strip() == "N":
             clear_screen()
             print("Come back when you are ready")
-            time.sleep(1.5)
+            time.sleep(2)
             intro_text()
         else:
             clear_screen()
             print("Type Y or N")
-            time.sleep(1.5)
+            time.sleep(2)
             intro_text()
         
     def first_question():
+        clear_screen()
+        print("You smell smoke, but no one else is reacting. You should:\n")
+        print("A: Alert others of your concern and go outside right away.")
+        print("B: Do nothing. Since nobody else is reacting, why should you.")
+        print("C: Wait for instructions.")
+
+        time.sleep(1)
+
+        user_choice1 = input("A - B - C: \n")
+
+        if user_choice1.upper().strip() == "A":
+            clear_screen()
+            print(random.choice(POSITIVE))
+            quotes_1()
+            time.sleep(1.5)
+            second_question()
+        elif user_choice1.upper().strip() == "B":
+            clear_screen()
+            print(random.choice(NEGATIVE))
+            quotes_1()
+            time.sleep(1.5)
+            first_question()
+        elif user_choice1.upper().strip() == "C":
+            clear_screen()
+            print(random.choice(NEGATIVE))
+            quotes_1()
+            time.sleep(1.5)
+            first_question()
+        else:
+            clear_screen()
+            print("Type A, B or C.")
+            time.sleep(1.5)
+            first_question()
+
+    def second_question():
         pass
+
 
     def clear_screen():
         '''
@@ -48,4 +92,4 @@ def run_Game():
     intro_text()
 
 
-run_Game()
+run_game()
